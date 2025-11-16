dddee08b4bc260b8de81bcd7a4efa2a104f6cfa9
Andrew Pantuso <ajpantuso@gmail.com>
2025-11-16 10:51:08 -0500

    chore: add cluster context setup

--- flake.nix
+++ flake.nix
@@ -40,7 +40,18 @@
           shellHook = ''
             export PROJECT_ROOT="$(git rev-parse --show-toplevel)";
 
-            kubectl config use-context pki
+            # Check if 'pki' context exists, if not create it
+            if ! kubectl config get-contexts | grep -q "pki"; then
+              kubectl config set-cluster pki --server=https://pki.ajphome.com:6443 --insecure-skip-tls-verify=true
+              kubectl config set-context pki --cluster=pki --user=ajpantuso@gmail.com
+            fi
+
+            # Test connectivity to the cluster
+            if kubectl cluster-info --context=pki &>/dev/null; then
+              kubectl config use-context pki
+            else
+              echo "Warning: Could not connect to pki cluster at https://pki.ajphome.com:6443"
+            fi
           '';
         };
       });
